aa65e8f50373a0683c76e5db9c2ea2f42eabb126
Diyan21 <diyanaxl@gmail.com>
2026-08-18 11:26:20 +0700

    Add files via upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Hero } from '../components/Hero';
+import { Brands } from '../components/Brands';
 import { TrustStats } from '../components/TrustStats';
 import { Features } from '../components/Features';
 import { About } from '../components/About';
@@ -19,7 +20,10 @@ export const Home: React.FC = () => {
       {/* 1. Hero Section */}
       <Hero />
 
-      {/* 2. Trust Indicators */}
+      {/* 2. Harga Paket / Brand CCTV */}
+      <Brands />
+
+      {/* 3. Trust Indicators */}
       <TrustStats />
 
       {/* 3. Keunggulan (Mengapa Memilih PUSTEKINDO?) */}
@@ -28,20 +32,20 @@ export const Home: React.FC = () => {
       {/* 4. Tentang Kami (Lebih dari Sekadar Menjual CCTV) */}
       <About />
 
-      {/* 5. Visi & Misi - dibuat menyatu dengan bagian Tentang Kami */}
-      <VisionMission />
-
-      {/* 6. Tujuan Sistem (Sistem Keamanan yang Memberikan Manfaat Nyata) */}
+      {/* 5. Tujuan Sistem (Sistem Keamanan yang Memberikan Manfaat Nyata) */}
       <SystemGoals />
 
-      {/* 7. Produk (Hikvision Categories) */}
+      {/* 6. Produk (Hikvision Categories) */}
       <Products />
 
-      {/* 8. Remote Monitoring (Dark Navy Section with Diagram) */}
+      {/* 7. Remote Monitoring (Dark Navy Section with Diagram) */}
       <RemoteMonitoring />
 
-      {/* 9. Solusi Berdasarkan Kebutuhan (10 Sectors & 7 Use Cases) */}
+      {/* 8. Solusi Berdasarkan Kebutuhan (10 Sectors & 7 Use Cases) */}
       <Solutions />
+
+      {/* 9. Visi & Misi */}
+      <VisionMission />
 
       {/* 10. Klien (17 Verified Enterprise Clients) */}
       <Clients />
